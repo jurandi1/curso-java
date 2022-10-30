276fa51cc1db34f4f5415f49b5c8f863fb0c7086
Jurandi Junior <jurandijr20@gmail.com>
2022-10-30 18:23:14 -0300

    Comentários

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,31 @@
 package fundamentos;
-
+/**
+ * Essa classe representa... JAVADOC
+ * 
+ * @author Jurandi Junior
+ * @since JDK11
+ *
+ */
 public class PrimeiroPrograma {
 
+	/**
+	 * ...
+	 * @param args
+	 * 
+	 */
 	public static void main(String[] args) {
+		
+		//Uma sentença de código termina com ;
+		//Mais um comentário 
+		//Fim
 		System.out.println("Primeiro programa! Parte #1");
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3..
+		 */
 		System.out.println("Primeiro programa! Parte #2");
+		System.out.println("Fim"); //Aqui também funciona!
 	}
 }
