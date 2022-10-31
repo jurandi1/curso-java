a81b8a9aca2343082cddaeba151aaa700a824319
Jurandi Junior <jurandijr20@gmail.com>
2022-10-31 19:18:43 -0300

    Notação Ponto

---
+++ exercicios/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,30 @@
+package fundamentos;
+
+public class NotacaoPonto {
+	
+	public static void main(String[] args) {
+		
+		String s = "Bom dia X";
+		s = s.replace("X", "Jurandi");
+		s = s.toUpperCase();
+		s = s.concat("!!!");
+		
+		
+		System.out.println(s);
+		
+		String x = "Carlos".toUpperCase();
+		System.out.println(x);
+		
+		String y = "Bom dia X"
+				.replace("X", "Junior")
+				.toUpperCase()
+				.concat("!!!");
+		System.out.println(y);
+		
+		// Tipos primitivos não tem o operador "."
+		int a = 3;
+		System.out.println(a);
+		
+	}
+
+}
