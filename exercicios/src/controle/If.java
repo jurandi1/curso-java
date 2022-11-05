267f2bd4c81e70cf2c424306dcf7a0aecb17d4d4
Jurandi Junior <jurandijr20@gmail.com>
2022-11-05 18:01:45 -0300

    If

---
+++ exercicios/src/controle/If.java
@@ -0,0 +1,31 @@
+package controle;
+
+import java.util.Scanner;
+
+public class If {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Informe a média: ");
+		double media = sc.nextDouble();
+		
+		if(media <= 10 && media >= 7.0) {
+			System.out.println("Aprovado!");
+			System.out.println("Parabéns!");
+		}
+		
+		if(media < 7 && media >= 4.5) {
+			System.out.println("Recuperação");
+		}
+		
+		boolean criterioDeReprovacao = media < 4.5 && media >= 0;
+		
+		if(criterioDeReprovacao) {
+			System.out.println("Reprovado");
+		}
+		
+		sc.close();
+	}
+}
