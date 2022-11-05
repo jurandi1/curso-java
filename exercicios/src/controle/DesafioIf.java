e69b0981418d3d41e61acde133a708531ca04fea
Jurandi Junior <jurandijr20@gmail.com>
2022-11-05 18:08:57 -0300

    Desafio If

---
+++ exercicios/src/controle/DesafioIf.java
@@ -0,0 +1,22 @@
+package controle;
+
+public class DesafioIf {
+	
+	public static void main(String[] args) {
+		
+		double nota = 1.3;
+		
+		/*
+		  	if(nota >= 9.0); {
+			System.out.println("Quadro de Honra!");
+			System.out.println("Você é fera!!!");
+		}
+		*/
+		
+		// Não usar ";" nas estruturas de controle (Apenas uma exceção)
+		if(nota >= 9.0) {
+			System.out.println("Quadro de Honra!");
+			System.out.println("Você é fera!!!");
+		}
+	}
+}
